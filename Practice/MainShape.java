c0b05f320764231be0d568dfca6b8934ae1bf6c9
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-25 21:59:50 +0530

    Create MainShape.java

---
+++ Practice/MainShape.java
@@ -0,0 +1,13 @@
+package com.inheritance;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		Square s = new Square ();
+		s.display();
+		Circle c = new Circle();
+		c.display();
+		
+	}
+}
